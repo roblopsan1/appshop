e47eb5ce061298cd53970f9091d60f1c2c9ef3e3
RobertNesta <roblopsan1@hotmail.com>
2022-05-20 11:54:01 -0500

    Se implemento diseño grafico y se crearon modelos category y producto

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,69 +1,159 @@
 @extends('layouts.app')
 
+@section('body-class', 'signup-page')
+
+
+
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Login</div>
-
-                <div class="panel-body">
-                    <form class="form-horizontal" method="POST" action="{{ route('login') }}">
-                        {{ csrf_field() }}
-
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-8 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Login
-                                </button>
-
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    Forgot Your Password?
+<div class="header header-filter" style="background-image: url('{{ asset('img/city.jpg') }}'); background-size: cover; background-position: top center;">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-3">
+                <div class="card card-signup">
+                    <form class="form"  method="POST" action="{{ route('login') }}">
+                    {{ csrf_field() }}
+                        <div class="header header-primary text-center">
+                            <h4>Inicio de sesión</h4>
+                            <!-- <div class="social-line">
+                                <a href="#pablo" class="btn btn-simple btn-just-icon">
+                                    <i class="fa fa-facebook-square"></i>
                                 </a>
-                            </div>
+                                <a href="#pablo" class="btn btn-simple btn-just-icon">
+                                    <i class="fa fa-twitter"></i>
+                                </a>
+                                <a href="#pablo" class="btn btn-simple btn-just-icon">
+                                    <i class="fa fa-google-plus"></i>
+                                </a>
+                            </div> -->
                         </div>
+                        <p class="text-divider">Ingresa tus datos</p>
+                        <div class="content">
+
+                            <div class="input-group">
+                                <span class="input-group-addon">
+                                    <i class="material-icons">email</i>
+                                </span>
+                                <input id="email" type="email" class="form-control" name="email"  placeholder="Email..."
+                                value="{{ old('email') }}" required autofocus>
+
+                            </div>
+
+                            <div class="input-group">
+                                <span class="input-group-addon">
+                                    <i class="material-icons">lock_outline</i>
+                                </span>
+                                <input id="password" type="password" class="form-control" name="password"  placeholder="Password..." required>
+
+                            </div>
+
+                        
+									<div class="checkbox">
+										<label>
+											<input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}>
+											Recordar sesión
+										</label>
+									</div>
+                        </div>
+                             <!-- <a class="btn btn-link" href="{{ route('password.request') }}">
+                Olvidaste tu contraseña
+            </a> -->
+                        <div class="footer text-center">
+                            <button type="submit"  class="btn btn-simple btn-primary btn-lg">Iniciar sesión</button>
+                        </div>
+                   
                     </form>
                 </div>
             </div>
         </div>
     </div>
+
+    <footer class="footer">
+        <div class="container">
+            <nav class="pull-left">
+                <ul>
+                    <li>
+                        <a href="http://www.creative-tim.com">
+                            Creative Tim
+                        </a>
+                    </li>
+                    <li>
+                        <a href="http://presentation.creative-tim.com">
+                            About Us
+                        </a>
+                    </li>
+                    <li>
+                        <a href="http://blog.creative-tim.com">
+                            Blog
+                        </a>
+                    </li>
+                    <li>
+                        <a href="http://www.creative-tim.com/license">
+                            Licenses
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+            <div class="copyright pull-right">
+                &copy; 2016, made with <i class="fa fa-heart heart"></i> by <a href="http://www.creative-tim.com" target="_blank">Creative Tim</a>
+            </div>
+        </div>
+    </footer>
+
 </div>
+
 @endsection
+
+
+<!-- 
+
+<form class="form-horizontal">
+ 
+
+    <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+        <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+
+        <div class="col-md-6">
+           
+            @if ($errors->has('email'))
+                <span class="help-block">
+                    <strong>{{ $errors->first('email') }}</strong>
+                </span>
+            @endif
+        </div>
+    </div>
+
+    <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+        <label for="password" class="col-md-4 control-label">Password</label>
+
+        <div class="col-md-6">
+
+            @if ($errors->has('password'))
+                <span class="help-block">
+                    <strong>{{ $errors->first('password') }}</strong>
+                </span>
+            @endif
+        </div>
+    </div>
+
+    <div class="form-group">
+        <div class="col-md-6 col-md-offset-4">
+            <div class="checkbox">
+                <label>
+                    <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
+                </label>
+            </div>
+        </div>
+    </div>
+
+    <div class="form-group">
+        <div class="col-md-8 col-md-offset-4">
+            <button type="submit" class="btn btn-primary">
+                Login
+            </button>
+
+            
+        </div>
+    </div>
+</form>
+    
+            -->
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,15 +1,107 @@
 @extends('layouts.app')
+@section('body-class', 'signup-page')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Register</div>
-
-                <div class="panel-body">
-                    <form class="form-horizontal" method="POST" action="{{ route('register') }}">
+<div class="header header-filter" style="background-image: url('{{ asset('img/city.jpg') }}'); background-size: cover; background-position: top center;">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-3">
+                <div class="card card-signup">
+                    <form class="form"  method="POST" action="{{ route('register') }}">
                         {{ csrf_field() }}
+                        <div class="header header-primary text-center">
+                            <h4>Registro</h4>
+                            <!-- <div class="social-line">
+                                <a href="#" class="btn btn-simple btn-just-icon">
+                                    <i class="fa fa-facebook-square"></i>
+                                </a>
+                                <a href="#" class="btn btn-simple btn-just-icon">
+                                    <i class="fa fa-twitter"></i>
+                                </a>
+                                <a href="#" class="btn btn-simple btn-just-icon">
+                                    <i class="fa fa-google-plus"></i>
+                                </a>
+                            </div> -->
+                        </div>
+                        <p class="text-divider">Completa tus datos</p>
+                        <div class="content">
+                        <div class="input-group">
+										<span class="input-group-addon">
+											<i class="material-icons">face</i>
+										</span>
+										<input type="text"  class="form-control" placeholder="Nombre" name="name" value="{{ old('name') }}"  required autofocus>
+									</div>
+                            <div class="input-group">
+                                <span class="input-group-addon">
+                                    <i class="material-icons">email</i>
+                                </span>
+                                <input id="email" type="email" class="form-control" name="email"  placeholder="Correo electrónico"
+                                value="{{ old('email') }}" required autofocus>
+
+                            </div>
+
+                            <div class="input-group">
+                                <span class="input-group-addon">
+                                    <i class="material-icons">lock_outline</i>
+                                </span>
+                                <input id="password" type="password" class="form-control" name="password"  placeholder="Contraseña" required>
+                            </div>
+                            <div class="input-group">
+                                <span class="input-group-addon">
+                                    <i class="material-icons">lock_outline</i>
+                                </span>
+                                <input id="password-confirm" type="password" class="form-control"  name="password_confirmation"  placeholder="Confirmar contraseña" required>
+                            </div>
+                        </div>
+                             <!-- <a class="btn btn-link" href="{{ route('password.request') }}">
+                Olvidaste tu contraseña
+            </a> -->
+                        <div class="footer text-center">
+                            <button type="submit"  class="btn btn-simple btn-primary btn-lg">Confirmar registro</button>
+                        </div>
+                   
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <footer class="footer">
+        <div class="container">
+            <nav class="pull-left">
+                <ul>
+                    <li>
+                        <a href="http://www.creative-tim.com">
+                            Creative Tim
+                        </a>
+                    </li>
+                    <li>
+                        <a href="http://presentation.creative-tim.com">
+                            About Us
+                        </a>
+                    </li>
+                    <li>
+                        <a href="http://blog.creative-tim.com">
+                            Blog
+                        </a>
+                    </li>
+                    <li>
+                        <a href="http://www.creative-tim.com/license">
+                            Licenses
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+            <div class="copyright pull-right">
+                &copy; 2016, made with <i class="fa fa-heart heart"></i> by <a href="http://www.creative-tim.com" target="_blank">Creative Tim</a>
+            </div>
+        </div>
+    </footer>
+
+</div>
+@endsection
+<!-- 
+<form class="form-horizontal" 
 
                         <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
                             <label for="name" class="col-md-4 control-label">Name</label>
@@ -68,10 +160,4 @@
                                 </button>
                             </div>
                         </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-@endsection
+                    </form> -->
